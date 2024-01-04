cfb4fe09bc1ef6ebb561fcd622eba0236f4d1db5
garymiller30 <garymiller30@gmail.com>
2024-01-04 11:12:13 +0200

    fix start programm from job list panel

--- ActiveWorks/Form2.cs
+++ ActiveWorks/Form2.cs
@@ -21,7 +21,7 @@ namespace ActiveWorks
 {
     public sealed partial class Form2 : KryptonForm
     {
-        private readonly string _version = $"{Localize.FormTitle} 8.6.8";
+        private readonly string _version = $"{Localize.FormTitle} 8.6.9";
         readonly List<FormProfile> _profileTabs = new List<FormProfile>();
 
         FormBackgroundTasks _formBackgroundTask;
