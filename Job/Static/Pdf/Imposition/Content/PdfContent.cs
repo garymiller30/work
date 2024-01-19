ccc2f48e419640c49316bee32d78064e4b0d7495
garymiller30 <garymiller30@gmail.com>
2024-01-19 12:20:54 +0200

    adding files

---
+++ Job/Static/Pdf/Imposition/Content/PdfContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Static.Pdf.Imposition.Content
+{
+    public class PdfContent
+    {
+        public void Add(string pdfFile)
+        {
+
+        }
+    }
+}
---
+++ Job/Static/Pdf/Imposition/Scheme/PdfSchemePage.cs
@@ -0,0 +1,20 @@
+﻿using Job.Static.Pdf.Imposition.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Static.Pdf.Imposition.Scheme
+{
+    public class PdfSchemePage
+    {
+        public int RowIdx { get;set; }
+        public int ColumnIdx { get;set; }
+        public Fields Gaps { get;set; } = new Fields();
+        public PdfSchemePageSide Front {  get; set; } = new PdfSchemePageSide();
+        public PdfSchemePageSide Back { get; set; } = new PdfSchemePageSide();
+
+       
+    }
+}
