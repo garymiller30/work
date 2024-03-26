4e8fa20375dfb434cbeede3fe2f92dd2fe049b89
garymiller30 <garymiller30@gmail.com>
2024-03-26 23:18:50 +0200

    color log rows

--- ActiveWorks/Form2.cs
+++ ActiveWorks/Form2.cs
@@ -20,7 +20,7 @@ namespace ActiveWorks
 {
     public sealed partial class Form2 : KryptonForm
     {
-        private readonly string _version = $"{Localize.FormTitle} 8.11.13";
+        private readonly string _version = $"{Localize.FormTitle} 8.12.13";
         readonly List<FormProfile> _profileTabs = new List<FormProfile>();
 
         FormBackgroundTasks _formBackgroundTask;
--- Logger/FormLogger.cs
+++ Logger/FormLogger.cs
@@ -12,6 +12,7 @@ namespace Logger
         public FormLogger()
         {
             InitializeComponent();
+            objectListView1.Sort(olvColumnDate, SortOrder.Descending);
         }
 
         internal void WriteLine(LogRow logRow)
@@ -29,6 +30,26 @@ namespace Logger
                 objectListView1.ClearObjects();    
             }
             
+        }
+
+        private void objectListView1_FormatRow(object sender, BrightIdeasSoftware.FormatRowEventArgs e)
+        {
+            LogRow row = e.Model as LogRow;
+            if (row.Status == "ERROR")
+            {
+                e.Item.BackColor = System.Drawing.Color.Red;
+                e.Item.ForeColor = System.Drawing.Color.White;
+            }
+            else if (row.Status == "WARNING")
+            {
+                e.Item.BackColor = System.Drawing.Color.Yellow;
+                e.Item.ForeColor = System.Drawing.Color.Black;
+            }
+            else if (row.Status == "INFO")
+            {
+                e.Item.BackColor = System.Drawing.Color.LightGray;
+                e.Item.ForeColor = System.Drawing.Color.Black;
+            }
         }
     }
 }
