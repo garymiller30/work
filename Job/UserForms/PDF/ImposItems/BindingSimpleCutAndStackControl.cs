a071ae0111f18203143e39e7c14b38803fe2c41e
garymiller30 <garymiller30@gmail.com>
2024-12-24 09:00:53 +0200

    fix cutandStack

--- Job/UserForms/PDF/ImposItems/BindingSimpleCutAndStackControl.cs
+++ Job/UserForms/PDF/ImposItems/BindingSimpleCutAndStackControl.cs
@@ -24,34 +24,50 @@ namespace JobSpace.UserForms.PDF.ImposItems
 
             for (int i = 0; i < sheets.Count; i++)
             {
+                int sides = sheets[i].SheetPlaceType == TemplateSheetPlaceType.SingleSide ? 1 : 2;
+
                 foreach (var t_page in sheets[i].TemplatePageContainer.TemplatePages)
                 {
-                    if (t_page.MasterFrontIdx > 0)
-                    {
-                        var pageIdx = i + (t_page.MasterFrontIdx - 1) * sheets.Count;
-                        t_page.PrintFrontIdx = pageIdx+1;
-                        if (pageIdx < pages.Count)
-                            pages[pageIdx].IsAssumed = true;
-                    }
-                    else
-                    {
-                        t_page.PrintFrontIdx = 0;
-                    }
+                    var pageIdx = i*sides  + (t_page.MasterFrontIdx - 1) * sheets.Count;
+                    t_page.PrintFrontIdx = pageIdx + 1;
+                    if (pageIdx < pages.Count) pages[pageIdx].IsAssumed = true;
 
                     if (t_page.MasterBackIdx > 0)
                     {
-                        var pageIdx = i + (t_page.MasterBackIdx - 1) * sheets.Count;
-                        t_page.PrintBackIdx = pageIdx+1;
-                        if (pageIdx < pages.Count)
-                            pages[pageIdx].IsAssumed = true;
+                        pageIdx++;
+                        t_page.PrintBackIdx = pageIdx + 1;
+                        if (pageIdx < pages.Count) pages[pageIdx].IsAssumed = true;
                     }
                     else
                     {
                         t_page.PrintBackIdx = 0;
                     }
+                    //if (t_page.MasterFrontIdx > 0)
+                    //{
+                    //    var pageIdx = i + (t_page.MasterFrontIdx - 1) * sheets.Count;
+                    //    t_page.PrintFrontIdx = pageIdx+1;
+                    //    if (pageIdx < pages.Count)
+                    //        pages[pageIdx].IsAssumed = true;
+                    //}
+                    //else
+                    //{
+                    //    t_page.PrintFrontIdx = 0;
+                    //}
+
+                    //if (t_page.MasterBackIdx > 0)
+                    //{
+                    //    var pageIdx = i + (t_page.MasterBackIdx - 1) * sheets.Count;
+                    //    t_page.PrintBackIdx = pageIdx+1;
+                    //    if (pageIdx < pages.Count)
+                    //        pages[pageIdx].IsAssumed = true;
+                    //}
+                    //else
+                    //{
+                    //    t_page.PrintBackIdx = 0;
+                    //}
 
                 }
-                
+
             }
         }
     }
