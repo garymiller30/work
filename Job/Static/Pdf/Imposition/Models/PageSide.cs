e294d3f3a8d903a929a65ca9d98d1a36ac000052
garymiller30 <garymiller30@gmail.com>
2025-01-20 11:23:53 +0200

    Add PageSide.cs with PageSide class and properties
    A new file `PageSide.cs` has been added to the project. This file includes several `using` directives for namespaces such as `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, `System.Text.Json.Serialization`, and `System.Threading.Tasks`.
    
    A new namespace `JobSpace.Static.Pdf.Imposition.Models` has been introduced. Within this namespace, a new public class `PageSide` has been defined. The `PageSide` class contains the following properties:
    - `MasterIdx` (int)
    - `PrintIdx` (int)
    - `AssignedRunPage` (of type `ImposRunPage`) which is decorated with the `[JsonIgnore]` attribute.
    - `Angle` (double) with a default value of 0.
    - `X` (double) with a default value of 0.
    - `Y` (double) with a default value of 0.

---
+++ Job/Static/Pdf/Imposition/Models/PageSide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JobSpace.Static.Pdf.Imposition.Models
+{
+    public class PageSide
+    {
+        public int MasterIdx { get; set; }
+        public int PrintIdx { get; set; }
+        
+        [JsonIgnore]
+        public ImposRunPage AssignedRunPage { get; set; }
+
+        public double Angle { get; set; } = 0;
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
+
+    }
+}
