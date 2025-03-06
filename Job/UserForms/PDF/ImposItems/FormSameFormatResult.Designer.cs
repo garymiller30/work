60196b59bf79863ceb7d825fcd61e2d2671a0730
garymiller30 <garymiller30@gmail.com>
2025-03-06 17:55:37 +0200

    Enhance error handling and UI in PDF processing forms
    Improved error messaging in `FileUtil.cs` to include `_result` in exceptions.
    Added new lists for managing print sheets and pages in `BindingSimpleOneFormatDifferentCount.cs`, along with an `ApplyChanges()` method for better result handling.
    Updated `CalcResult` class to track available spaces with a new `FreeCount` property.
    Enhanced UI in `FormSameFormatResult.Designer.cs` with new components for free space display and context menu items for page count adjustments.
    Updated `FormSameFormatResult.cs` to manage results and UI interactions more effectively, including new event handlers.
    Modified `FormSameFormatResult.resx` to include metadata for the new context menu strip.

--- Job/UserForms/PDF/ImposItems/FormSameFormatResult.Designer.cs
+++ Job/UserForms/PDF/ImposItems/FormSameFormatResult.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
             this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
@@ -37,7 +38,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.tb_freeSpace = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_close_and_apply = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // objectListView1
@@ -55,14 +64,16 @@
             this.olvColumn2,
             this.olvColumn3,
             this.olvColumn4});
+            this.objectListView1.ContextMenuStrip = this.contextMenuStrip1;
             this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
             this.objectListView1.FullRowSelect = true;
             this.objectListView1.GridLines = true;
             this.objectListView1.HideSelection = false;
             this.objectListView1.Location = new System.Drawing.Point(12, 42);
+            this.objectListView1.MultiSelect = false;
             this.objectListView1.Name = "objectListView1";
             this.objectListView1.ShowGroups = false;
-            this.objectListView1.Size = new System.Drawing.Size(650, 283);
+            this.objectListView1.Size = new System.Drawing.Size(650, 246);
             this.objectListView1.TabIndex = 0;
             this.objectListView1.UseCompatibleStateImageBehavior = false;
             this.objectListView1.View = System.Windows.Forms.View.Details;
@@ -97,7 +108,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(283, 14);
+            this.label1.Location = new System.Drawing.Point(230, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 13);
             this.label1.TabIndex = 1;
@@ -105,7 +116,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(394, 11);
+            this.textBox1.Location = new System.Drawing.Point(341, 11);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(100, 20);
@@ -113,7 +124,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(129, 14);
+            this.textBox2.Location = new System.Drawing.Point(112, 11);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(100, 20);
@@ -122,17 +133,80 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 17);
+            this.label2.Location = new System.Drawing.Point(12, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "загальний тираж";
+            // 
+            // tb_freeSpace
+            // 
+            this.tb_freeSpace.Location = new System.Drawing.Point(543, 11);
+            this.tb_freeSpace.Name = "tb_freeSpace";
+            this.tb_freeSpace.ReadOnly = true;
+            this.tb_freeSpace.Size = new System.Drawing.Size(100, 20);
+            this.tb_freeSpace.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "вільних місць";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(89, 48);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(88, 22);
+            this.toolStripMenuItem2.Text = "+1";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(88, 22);
+            this.toolStripMenuItem3.Text = "-1";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(533, 302);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(129, 23);
+            this.btn_close.TabIndex = 8;
+            this.btn_close.Text = "закрити без змін";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_close_and_apply
+            // 
+            this.btn_close_and_apply.Location = new System.Drawing.Point(12, 302);
+            this.btn_close_and_apply.Name = "btn_close_and_apply";
+            this.btn_close_and_apply.Size = new System.Drawing.Size(146, 23);
+            this.btn_close_and_apply.TabIndex = 9;
+            this.btn_close_and_apply.Text = "прийняти зміни і закрити";
+            this.btn_close_and_apply.UseVisualStyleBackColor = true;
+            this.btn_close_and_apply.Click += new System.EventHandler(this.btn_close_and_apply_Click);
             // 
             // FormSameFormatResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(674, 337);
+            this.Controls.Add(this.btn_close_and_apply);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.tb_freeSpace);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -143,6 +217,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "звіт";
             ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,5 +234,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_freeSpace;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_close_and_apply;
     }
 }
--- Job/UserForms/PDF/ImposItems/FormSameFormatResult.cs
+++ Job/UserForms/PDF/ImposItems/FormSameFormatResult.cs
@@ -13,17 +13,76 @@ namespace JobSpace.UserForms.PDF.ImposItems
 {
     public partial class FormSameFormatResult : Form
     {
+        CalcResult _result;
+
         public FormSameFormatResult()
         {
             InitializeComponent();
+            DialogResult = DialogResult.Cancel;
         }
 
         internal void SetResult(CalcResult result)
         {
-            textBox1.Text = result.SheetCount.ToString();
-            textBox2.Text = result.TotalCount.ToString();
+            _result = result;
+            SetInfo();
 
             objectListView1.AddObjects(result.Files);
+        }
+
+        private void SetInfo()
+        {
+            textBox1.Text = _result.SheetCount.ToString();
+            textBox2.Text = _result.TotalCount.ToString();
+            tb_freeSpace.Text = _result.FreeCount.ToString();
+        }
+
+        //+1
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (objectListView1.SelectedObject is FileResult info)
+            {
+                if (_result.FreeCount == 0) {
+
+                    MessageBox.Show("Немає вільних місць на листі");
+                    return;
+                }
+
+                _result.FreeCount--;
+                info.PagesOnSheet++;
+
+                _result.Recalc();
+
+                objectListView1.RefreshObjects(_result.Files);
+                SetInfo();
+            }
+        }
+        //-1
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (objectListView1.SelectedObject is FileResult info)
+            {
+                if (info.PagesOnSheet == 1){
+                    MessageBox.Show("Видалення останньої позиції з листа неможливе");
+                    return; 
+                    }
+
+                _result.FreeCount++;
+                info.PagesOnSheet--;
+                _result.Recalc();
+                objectListView1.RefreshObjects(_result.Files);
+                SetInfo();
+            }
+        }
+
+        private void btn_close_and_apply_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+            Close();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
