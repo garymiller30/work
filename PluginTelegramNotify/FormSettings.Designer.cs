e36d0b213f648a1cc66991c907db0af1fed7b27f
garymiller30 <garymiller30@gmail.com>
2025-07-26 15:03:15 +0300

    adding settings dialog

---
+++ PluginTelegramNotify/FormSettings.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PluginTelegramNotify
+{
+    partial class FormSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_bot_id = new System.Windows.Forms.TextBox();
+            this.tb_chat_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_bot_id
+            // 
+            this.tb_bot_id.Location = new System.Drawing.Point(12, 36);
+            this.tb_bot_id.Name = "tb_bot_id";
+            this.tb_bot_id.Size = new System.Drawing.Size(334, 20);
+            this.tb_bot_id.TabIndex = 0;
+            // 
+            // tb_chat_id
+            // 
+            this.tb_chat_id.Location = new System.Drawing.Point(12, 91);
+            this.tb_chat_id.Name = "tb_chat_id";
+            this.tb_chat_id.Size = new System.Drawing.Size(334, 20);
+            this.tb_chat_id.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bot ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chat ID";
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(141, 139);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 34);
+            this.btn_ok.TabIndex = 4;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // FormSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 183);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_chat_id);
+            this.Controls.Add(this.tb_bot_id);
+            this.Name = "FormSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Налаштування";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_bot_id;
+        private System.Windows.Forms.TextBox tb_chat_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_ok;
+    }
+}
