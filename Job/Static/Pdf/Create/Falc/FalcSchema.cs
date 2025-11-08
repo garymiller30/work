61f7949f053d970b69badbd1476bf30aa7e43516
garymiller30 <garymiller30@gmail.com>
2025-11-08 15:08:29 +0200

    Add Falc schema generation functionality
    Introduced `FalcSchema` and `FalcSchemaParams` classes to enable
    Falc schema creation using the `PDFlib` library. Updated `Job.csproj`
    to include these new classes.
    
    Enhanced the `FormVisualFalc` UI by adding a "create schema" button
    (`btn_create_schema`) and implemented its event handler to generate
    Falc schemas based on user input.
    
    Added logic in `FalcSchema` to process PDF page information, apply
    mirrored and non-mirrored logic, and save the generated schema as a
    new PDF file. Included error handling and resource cleanup.
    
    Updated namespaces and imports to integrate the new functionality
    seamlessly into the existing application.

---
+++ Job/Static/Pdf/Create/Falc/FalcSchema.cs
@@ -0,0 +1,109 @@
+﻿using JobSpace.Static.Pdf.Common;
+using JobSpace.Static.Pdf.Imposition.Models;
+using PDFlib_dotnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSpace.Static.Pdf.Create.Falc
+{
+    public class FalcSchema
+    {
+        FalcSchemaParams _param;
+
+        public FalcSchema(FalcSchemaParams param)
+        {
+            _param = param;
+        }
+
+        public void Run(string filePath)
+        {
+            var boxes = PdfHelper.GetPagesInfo(filePath);
+
+            // Логіка створення схеми для Falc на основі boxes та _param
+            PDFlib p = new PDFlib();
+
+            try
+            {
+                string targetfile = System.IO.Path.Combine(
+                    System.IO.Path.GetDirectoryName(filePath),
+                    System.IO.Path.GetFileNameWithoutExtension(filePath) + "_falc_schema" + System.IO.Path.GetExtension(filePath)
+                    );
+
+                MarkColor markColor = MarkColor.ProofColor;
+
+                p.begin_document(targetfile, "optimize=true");
+                foreach (var pageInfo in boxes)
+                {
+                    p.begin_page_ext(pageInfo.Mediabox.width, pageInfo.Mediabox.height, "");
+
+                    int gstate = p.create_gstate("overprintmode=1 overprintfill=true overprintstroke=true");
+                    p.set_gstate(gstate);
+
+                    p.setcolor("fillstroke", "cmyk", markColor.C/100, markColor.M/100, markColor.Y/100, markColor.K/100);
+                    int spot = p.makespotcolor(markColor.Name);
+                    p.setlinewidth(1.0);
+                    p.rect(pageInfo.Trimbox.left, pageInfo.Trimbox.bottom, pageInfo.Trimbox.width, pageInfo.Trimbox.height);
+                    p.stroke();
+
+                    double xOfs = pageInfo.Trimbox.left;
+                    double yOfs = pageInfo.Trimbox.bottom;
+
+                    if (_param.Mirrored) {
+                        // Логіка для дзеркальних частин
+                        //отримати індекс поточної частини
+                        
+                        int idx = boxes.IndexOf(pageInfo);
+                        if (idx % 2 == 1)
+                        {
+                            // Логіка для парних частин
+                            for (int i = 0; i < _param.PartsWidth.Length-1; i++)
+                            {
+                                xOfs += (double)_param.PartsWidth[i] *  PdfHelper.mn ;
+                                p.moveto(xOfs, yOfs);
+                                p.lineto(xOfs, yOfs + pageInfo.Trimbox.height);
+                                p.stroke();
+                            }
+                        }
+                        else
+                        {
+                            // Логіка для непарних частин
+                            for (int i = _param.PartsWidth.Length - 1; i > 0; i--)
+                            {
+                                xOfs += (double)_param.PartsWidth[i] * PdfHelper.mn;
+                                p.moveto(xOfs, yOfs);
+                                p.lineto(xOfs, yOfs + pageInfo.Trimbox.height);
+                                p.stroke();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Логіка для звичайних частин
+                        for (int i = _param.PartsWidth.Length - 1; i > 0; i--)
+                        {
+                            xOfs += (double)_param.PartsWidth[i] * PdfHelper.mn;
+                            p.moveto(xOfs, yOfs);
+                            p.lineto(xOfs, yOfs + pageInfo.Trimbox.height);
+                            p.stroke();
+                        }
+                    }
+                    // Тут має бути логіка створення схеми Falc
+                    // Використовуйте _param.Mirrored та _param.PartsWidth для налаштування схеми
+                    p.end_page_ext("");
+                }
+                p.end_document("");
+            }
+            catch (PDFlibException e)
+            {
+                PdfHelper.LogException(e, "FalcSchema");
+            }
+            finally
+            {
+                p?.Dispose();
+            }
+        }
+    }
+}
