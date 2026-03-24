2b92bc702d2a3f1344a61326b8d2731319518490
garymiller30 <garymiller30@gmail.com>
2026-03-24 19:40:00 +0200

    add

---
+++ Job/UserForms/PDF/Visual/FormVisualSaddleStitchCreep.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace JobSpace.UserForms.PDF.Visual
+{
+    partial class FormVisualSaddleStitchCreep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uc_PreviewBrowserFile1 = new JobSpace.UC.Uc_FilePreviewControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nud_paper_thickness = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_paper_thickness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uc_PreviewBrowserFile1
+            // 
+            this.uc_PreviewBrowserFile1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uc_PreviewBrowserFile1.Location = new System.Drawing.Point(270, 12);
+            this.uc_PreviewBrowserFile1.Name = "uc_PreviewBrowserFile1";
+            this.uc_PreviewBrowserFile1.Size = new System.Drawing.Size(798, 559);
+            this.uc_PreviewBrowserFile1.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.nud_paper_thickness);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 51);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Товщина паперу, мм";
+            // 
+            // nud_paper_thickness
+            // 
+            this.nud_paper_thickness.DecimalPlaces = 2;
+            this.nud_paper_thickness.Location = new System.Drawing.Point(191, 20);
+            this.nud_paper_thickness.Name = "nud_paper_thickness";
+            this.nud_paper_thickness.Size = new System.Drawing.Size(55, 20);
+            this.nud_paper_thickness.TabIndex = 0;
+            this.nud_paper_thickness.ValueChanged += new System.EventHandler(this.nud_paper_thickness_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FormVisualSaddleStitchCreep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 583);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.uc_PreviewBrowserFile1);
+            this.Name = "FormVisualSaddleStitchCreep";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Візуал виштовхування при шитві скобою";
+            this.Load += new System.EventHandler(this.FormVisualSaddleStitchCreep_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_paper_thickness)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UC.Uc_FilePreviewControl uc_PreviewBrowserFile1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nud_paper_thickness;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Job/UserForms/PDF/Visual/FormVisualSaddleStitchCreep.cs
@@ -0,0 +1,111 @@
+﻿using Amazon.Runtime.Internal.Transform;
+using Interfaces;
+using JobSpace.Models.ScreenPrimitives;
+using JobSpace.Static.Pdf.Common;
+using JobSpace.Static.Pdf.Imposition.Models.Marks.ColorControl.Primitives;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobSpace.UserForms.PDF.Visual
+{
+    public partial class FormVisualSaddleStitchCreep : Form
+    {
+        Pen big_pen = new Pen(Color.Red, 0.4f);
+        Pen white_pen = new Pen(Color.White, 0.6f);
+        IFileSystemInfoExt _fileSystemInfoExt;
+        List<IScreenPrimitive> _primitives = new List<IScreenPrimitive>();
+        int curPageNo = 0;
+
+        Dictionary<string,decimal> thicknesses = new Dictionary<string,decimal>(){
+            { "офсетний 80 г/м²",0.1m },
+            { "офсетний 90 г/м²",0.12m },
+            { "офсетний 115 г/м²",0.15m },
+            { "офсетний 130 г/м²",0.17m },
+            { "офсетний 150 г/м²",0.2m },
+            { "офсетний 170 г/м²",0.23m },
+            { "офсетний 200 г/м²",0.27m },
+            { "офсетний 250 г/м²",0.33m },
+            { "офсетний 300 г/м²",0.41m },
+            { "крейдований 80 г/м²",0.07m },
+            { "крейдований 90 г/м²",0.08m },
+            { "крейдований 115 г/м²",0.1m },
+            { "крейдований 130 г/м²",0.11m },
+            { "крейдований 150 г/м²",0.13m },
+            { "крейдований 170 г/м²",0.15m },
+            { "крейдований 200 г/м²",0.19m },
+            { "крейдований 250 г/м²",0.23m },
+            { "крейдований 300 г/м²",0.29m },
+            };
+
+        public FormVisualSaddleStitchCreep(IFileSystemInfoExt file)
+        {
+            InitializeComponent();
+            _fileSystemInfoExt = file;
+
+            uc_PreviewBrowserFile1.OnPageChanged += (s, pageIdx) =>
+            {
+                curPageNo = pageIdx;
+                Redraw();
+            };
+
+            comboBox1.Items.AddRange(thicknesses.Keys.ToArray());
+            comboBox1.SelectedIndex = 0;
+            
+        }
+
+        private void Redraw()
+        {
+            _primitives = new List<IScreenPrimitive>();
+            DrawLines();
+
+            uc_PreviewBrowserFile1.SetPrimitives(_primitives);
+        }
+
+        private void DrawLines()
+        {
+            var box = uc_PreviewBrowserFile1.GetCurrentPageInfo();
+            if (box == null) return;
+            int totalPages = uc_PreviewBrowserFile1.GetTotalPages();
+            double paperThickness = (double)nud_paper_thickness.Value;
+            var x = (float)CreepCalculator.GetCreepForPage(curPageNo, totalPages, paperThickness);
+            float h = (float)box.Trimbox.hMM();
+
+            if (curPageNo % 2 != 0) // парна
+            {
+                x = (float)box.Trimbox.wMM() - x;
+            }
+            _primitives.Add(new ScreenLine(white_pen, x, 0, x, h));
+            _primitives.Add(new ScreenLine(big_pen, x, 0, x, h));
+
+        }
+
+        private void FormVisualSaddleStitchCreep_Load(object sender, EventArgs e)
+        {
+            if (_fileSystemInfoExt != null)
+            {
+                uc_PreviewBrowserFile1.Show(_fileSystemInfoExt);
+            }
+        }
+
+        private void nud_paper_thickness_ValueChanged(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // виставити nud_paper_thickness
+            if (comboBox1.SelectedItem is string key)
+            {
+                nud_paper_thickness.Value = thicknesses[key];
+            }
+        }
+    }
+}
