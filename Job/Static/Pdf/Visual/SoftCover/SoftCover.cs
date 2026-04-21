f1ca7931c682ef4b726d36efcb13515f142c8293
garymiller30 <garymiller30@gmail.com>
2026-04-21 23:21:36 +0300

    refactor

--- Job/Static/Pdf/Visual/SoftCover/SoftCover.cs
+++ Job/Static/Pdf/Visual/SoftCover/SoftCover.cs
@@ -9,10 +9,22 @@ using System.Windows.Forms;
 
 namespace JobSpace.Static.Pdf.Visual.SoftCover
 {
-    [PdfTool("Візуалізація","М'яка обкладинка",Icon = "visual_soft_cover", Order = 20)]
+    [PdfTool("Візуалізація", "М'яка обкладинка", Icon = "visual_soft_cover", Order = 20)]
     public class SoftCover : IPdfTool
     {
+        const double COEF_DIMENSION = 0.3;
+
         SoftCoverParams _coverParams;
+
+        double totalWidth;
+        double totalHeight;
+        double width;
+        double height;
+        double bleed;
+        double leftKlapan;
+        double rightKlapan;
+        double root;
+
 
         public bool Configure(PdfJobContext context)
         {
@@ -24,12 +36,24 @@ namespace JobSpace.Static.Pdf.Visual.SoftCover
                     if (form.ShowDialog() == DialogResult.OK)
                     {
                         _coverParams = form.CoverParams;
+
+                        totalHeight = mn(_coverParams.TotalHeight);
+                        totalWidth = mn(_coverParams.TotalWidth);
+                        width = mn(_coverParams.Width);
+                        height = mn(_coverParams.Height);
+                        bleed = mn(_coverParams.Bleed);
+                        leftKlapan = mn(_coverParams.LeftKlapan);
+                        rightKlapan = mn(_coverParams.RightKlapan);
+                        root = mn(_coverParams.Root);
+
                         return true;
                     }
                 }
             }
             return false;
         }
+
+        double mn(double value) => value * PdfHelper.mn;
 
         public void Execute(PdfJobContext context)
         {
@@ -51,89 +75,88 @@ namespace JobSpace.Static.Pdf.Visual.SoftCover
                 var fi = new System.IO.FileInfo(file);
                 output_file = System.IO.Path.Combine(_coverParams.FolderOutput, $"{fi.Name}_+_schema.pdf");
             }
-            var p = new PDFlib();
-            try
+            using (var p = new PDFlib())
             {
-                p.begin_document(output_file, "optimize=true");
-                p.begin_page_ext(_coverParams.TotalWidth * PdfHelper.mn, _coverParams.TotalHeight * PdfHelper.mn, "");
-                int l_print = p.define_layer("print", "");
-                int v_layer = p.define_layer("visual", "");
-
-                if (_coverParams.Command == SoftCoverParams.CreateCommand.CreateSoftCoverWithFile)
+                try
                 {
-                    p.begin_layer(l_print);
-                    var doc = p.open_pdi_document(file, "");
-                    var page_handle = p.open_pdi_page(doc, 1, "");
-                    p.fit_pdi_page(page_handle, (_coverParams.TotalWidth / 2) * PdfHelper.mn, (_coverParams.TotalHeight / 2) * PdfHelper.mn, "position={center center}");
-                    p.close_pdi_page(page_handle);
-                    p.end_layer();
+                    p.begin_document(output_file, "optimize=true");
+                    p.begin_page_ext(totalWidth, totalHeight, "");
+                    int l_print = p.define_layer("print", "");
+                    int v_layer = p.define_layer("visual", "");
+
+                    if (_coverParams.Command == SoftCoverParams.CreateCommand.CreateSoftCoverWithFile)
+                    {
+                        p.begin_layer(l_print);
+                        var doc = p.open_pdi_document(file, "");
+                        var page_handle = p.open_pdi_page(doc, 1, "");
+                        p.fit_pdi_page(page_handle, totalWidth / 2, totalHeight / 2, "position={center center}");
+                        p.close_pdi_page(page_handle);
+                        p.end_layer();
+                    }
+
+                    p.begin_layer(v_layer);
+                    // Draw cover schema here
+                    int gstate = p.create_gstate("overprintmode=1 overprintfill=true overprintstroke=true");
+                    p.set_gstate(gstate);
+
+                    MarkColor c = MarkColor.ProofColor;
+                    p.setcolor("fillstroke", "cmyk", c.C / 100, c.M / 100, c.Y / 100, c.K / 100);
+                    int spot = p.makespotcolor(c.Name);
+                    // p.setdash(4, 2);
+                    p.set_graphics_option("dasharray={4 2}");
+                    p.setlinewidth(0.2);
+                    p.setcolor("fillstroke", "spot", spot, 1.0, 0, 0);
+
+                    // обрізний прямокутник
+                    double x = bleed;
+                    double y = bleed;
+                    double w = totalWidth - 2 * bleed;
+                    double h = totalHeight - 2 * bleed;
+
+                    p.rect(x, y, w, h);
+
+                    if (leftKlapan > 0)
+                    {
+                        x += leftKlapan;
+                        DrawLine(p, x, y, h);
+                    }
+
+                    x += width;
+                    DrawLine(p, x, y, h);
+
+                    x += root;
+                    DrawLine(p, x, y, h);
+
+                    x += width;
+                    DrawLine(p, x, y, h);
+                    if (rightKlapan > 0)
+                    {
+                        x += rightKlapan;
+                        DrawLine(p, x, y, h);
+                    }
+
+                    p.stroke();
+
+                    DrawDimensions(p, spot);
+
+                    x = bleed;
+                    y = bleed;
+                    w = totalWidth - x;
+                    h = totalHeight - y;
+
+                    p.end_page_ext($"trimbox {{{x} {y} {w} {h}}}");
+                    p.end_document("");
                 }
-
-                p.begin_layer(v_layer);
-                // Draw cover schema here
-                int gstate = p.create_gstate("overprintmode=1 overprintfill=true overprintstroke=true");
-                p.set_gstate(gstate);
-
-                MarkColor c = MarkColor.ProofColor;
-                p.setcolor("fillstroke", "cmyk", c.C / 100, c.M / 100, c.Y / 100, c.K / 100);
-                int spot = p.makespotcolor(c.Name);
-                // p.setdash(4, 2);
-                p.set_graphics_option("dasharray={4 2}");
-                p.setlinewidth(0.2);
-                p.setcolor("fillstroke", "spot", spot, 1.0, 0, 0);
-
-                // обрізний прямокутник
-                double x = _coverParams.Bleed;
-                double y = _coverParams.Bleed;
-                double w = _coverParams.TotalWidth - 2 * _coverParams.Bleed;
-                double h = _coverParams.TotalHeight - 2 * _coverParams.Bleed;
-                p.rect(x * PdfHelper.mn, y * PdfHelper.mn, w * PdfHelper.mn, h * PdfHelper.mn);
-
-                if (_coverParams.LeftKlapan > 0)
+                catch (PDFlibException e)
                 {
-                    x += _coverParams.LeftKlapan;
-                    p.moveto(x * PdfHelper.mn, y * PdfHelper.mn);
-                    p.lineto(x * PdfHelper.mn, (y + h) * PdfHelper.mn);
+                    Logger.Log.Error(null, "SoftCoverGenerator", $"[{e.get_errnum()}] {e.get_apiname()}: {e.get_errmsg()}");
                 }
-
-                x += _coverParams.Width;
-                p.moveto(x * PdfHelper.mn, y * PdfHelper.mn);
-                p.lineto(x * PdfHelper.mn, (y + h) * PdfHelper.mn);
-
-                x += _coverParams.Root;
-                p.moveto(x * PdfHelper.mn, y * PdfHelper.mn);
-                p.lineto(x * PdfHelper.mn, (y + h) * PdfHelper.mn);
-
-                x += _coverParams.Width;
-                p.moveto(x * PdfHelper.mn, y * PdfHelper.mn);
-                p.lineto(x * PdfHelper.mn, (y + h) * PdfHelper.mn);
-                if (_coverParams.RightKlapan > 0)
-                {
-                    x += _coverParams.RightKlapan;
-                    p.moveto(x * PdfHelper.mn, y * PdfHelper.mn);
-                    p.lineto(x * PdfHelper.mn, (y + h) * PdfHelper.mn);
-                }
-
-                p.stroke();
-
-                DrawDimensions(p, spot);
-
-                x = _coverParams.Bleed;
-                y = _coverParams.Bleed;
-                w = _coverParams.TotalWidth - x;
-                h = _coverParams.TotalHeight - y;
-
-                p.end_page_ext($"trimbox {{{x * PdfHelper.mn} {y * PdfHelper.mn} {w * PdfHelper.mn} {h * PdfHelper.mn}}}");
-                p.end_document("");
             }
-            catch (PDFlibException e)
-            {
-                Logger.Log.Error(null, "PdfSpliter", $"[{e.get_errnum()}] {e.get_apiname()}: {e.get_errmsg()}");
-            }
-            finally
-            {
-                p?.Dispose();
-            }
+        }
+        void DrawLine(PDFlib p, double x, double y, double h)
+        {
+            p.moveto(x, y);
+            p.lineto(x, y + h);
         }
 
         private void DrawDimensions(PDFlib p, int spot)
@@ -141,11 +164,11 @@ namespace JobSpace.Static.Pdf.Visual.SoftCover
             p.setlinewidth(0.1);
             p.set_graphics_option("dasharray=none");
 
-            double[] widths = new double[7] { _coverParams.Bleed,_coverParams.LeftKlapan,  _coverParams.Width, _coverParams.Root,  _coverParams.Width,_coverParams.RightKlapan, _coverParams.Bleed };
+            double[] widths = new double[7] { _coverParams.Bleed, _coverParams.LeftKlapan, _coverParams.Width, _coverParams.Root, _coverParams.Width, _coverParams.RightKlapan, _coverParams.Bleed };
             double[] heigts = new double[3] { _coverParams.Bleed, _coverParams.Height, _coverParams.Bleed };
 
             double x = 0;
-            double y = _coverParams.Bleed + _coverParams.Height * 0.3;
+            double y = _coverParams.Bleed + _coverParams.Height * COEF_DIMENSION;
 
             for (int i = 0; i < widths.Count(); i++)
             {
@@ -154,7 +177,7 @@ namespace JobSpace.Static.Pdf.Visual.SoftCover
                 x += widths[i];
             }
 
-            x = _coverParams.Bleed + _coverParams.Width * 0.3;
+            x = _coverParams.Bleed + _coverParams.Width * COEF_DIMENSION;
             y = 0;
 
             for (int i = 0; i < heigts.Count(); i++)
