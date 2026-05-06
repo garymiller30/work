02c2ef8b8f4a14414277550f674e299bf1dd0b9f
garymiller30 <garymiller30@gmail.com>
2026-05-06 10:44:03 +0300

    add

--- ActiveWorks/Properties/Settings.Designer.cs
+++ ActiveWorks/Properties/Settings.Designer.cs
@@ -160,7 +160,7 @@ namespace ActiveWorks.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool LicenseServerEnabled {
             get {
                 return ((bool)(this["LicenseServerEnabled"]));
