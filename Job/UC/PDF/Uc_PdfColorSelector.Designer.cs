0408ff013850b5faa652e1e4624baa45761e78da
garymiller30 <garymiller30@gmail.com>
2026-05-19 08:59:34 +0300

    Refresh job statuses after settings changes

---
+++ Job/UC/PDF/Uc_PdfColorSelector.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace JobSpace.UC.PDF
+{
+    partial class Uc_PdfColorSelector
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Uc_PdfColorSelector
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Name = "Uc_PdfColorSelector";
+            Size = new System.Drawing.Size(259, 71);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Job/UC/PDF/Uc_PdfColorSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JobSpace.UC.PDF
+{
+    public partial class Uc_PdfColorSelector : UserControl
+    {
+        public Uc_PdfColorSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
