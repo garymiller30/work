48a22d52ee6657683a40db26a487b7eb59de0ab1
garymiller30 <garymiller30@gmail.com>
2026-06-03 08:47:13 +0300

    chore: додайте using System.Threading в NoCache.cs

--- Job/UC/NoCache.cs
+++ Job/UC/NoCache.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Interfaces;
 using JobSpace.Models;
 using JobSpace.Static;
