7129a2ba0ce327bd32f37e856cf0603493a3b725
garymiller30 <garymiller30@gmail.com>
2026-06-08 08:46:43 +0300

    Refactor file handling and error management in PDF tools
    Improve file path checks, output naming, and exception handling in CreateBigovkaMarks.cs and FalcSchema.cs. Adopt modern C# syntax, centralize error logging, and enhance code readability with consistent formatting.

--- Job/Static/Pdf/Visual/Falc/FalcSchema.cs
+++ Job/Static/Pdf/Visual/Falc/FalcSchema.cs
@@ -5,6 +5,7 @@ using JobSpace.Static.Pdf.Imposition.Models;
 using JobSpace.UserForms.PDF.Visual;
 using PDFlib_dotnet;
 using System;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -46,94 +47,96 @@ namespace JobSpace.Static.Pdf.Create.Falc
                 }
                 if (_param.CreateFileAndSchema)
                 {
-                    CreateFalcSchema(file.FullName,true);
+                    CreateFalcSchema(file.FullName, true);
                 }
-                
             }
         }
 
-        public void CreateFalcSchema(string filePath,bool fileAndSchema = false)
+        public void CreateFalcSchema(string filePath, bool fileAndSchema = false)
         {
             var boxes = PdfHelper.GetPagesInfo(filePath);
 
             // Логіка створення схеми для Falc на основі boxes та _param
-            using (PDFlib p = new PDFlib())
+            using PDFlib p = new PDFlib();
+
+            try
             {
-                try
+                string sufix = fileAndSchema ? "_+_schema" : "_schema";
+
+                var directory = Path.GetDirectoryName(filePath);
+                if (string.IsNullOrEmpty(directory)) return;
+
+                string targetfile = Path.Combine(
+                    directory, $"{Path.GetFileNameWithoutExtension(filePath)}{sufix}.pdf");
+
+                MarkColor markColor = MarkColor.ProofColor;
+
+                p.begin_document(targetfile, "optimize=true");
+
+                int l_print = p.define_layer("print", "");
+                int v_layer = p.define_layer("visual", "");
+
+                int doc = -1;
+                if (fileAndSchema)
                 {
-                    string sufix = fileAndSchema ? "_+_schema" : "_schema";
+                    doc = p.open_pdi_document(filePath, "");
+                }
+                for (int idx = 0; idx < boxes.Count; idx++)
+                {
+                    PdfPageInfo pageInfo = boxes[idx];
 
-                    string targetfile = System.IO.Path.Combine(
-                        System.IO.Path.GetDirectoryName(filePath), $"{System.IO.Path.GetFileNameWithoutExtension(filePath)}{sufix}.pdf");
+                    p.begin_page_ext(pageInfo.Mediabox.width, pageInfo.Mediabox.height, "");
 
-                    MarkColor markColor = MarkColor.ProofColor;
-
-                    p.begin_document(targetfile, "optimize=true");
-
-                    int doc = -1;
                     if (fileAndSchema)
                     {
-                        doc = p.open_pdi_document(filePath, "");
+                        p.begin_layer(l_print);
+                        var page_handle = p.open_pdi_page(doc, idx + 1, "");
+                        p.fit_pdi_page(page_handle, 0, 0, "");
+                        p.close_pdi_page(page_handle);
+
+                        DrawFalcMarks(p, pageInfo, idx);
                     }
-                    for (int idx = 0; idx < boxes.Count; idx++)
+
+                    p.begin_layer(v_layer);
+                    int gstate = p.create_gstate("overprintmode=1 overprintfill=true overprintstroke=true");
+                    p.set_gstate(gstate);
+
+                    p.setcolor("fillstroke", "cmyk", markColor.C / 100, markColor.M / 100, markColor.Y / 100, markColor.K / 100);
+                    int spot = p.makespotcolor(markColor.Name);
+                    p.setcolor("stroke", "spot", spot, 1.0, 0.0, 0.0);
+                    p.setlinewidth(1.0);
+                    p.rect(pageInfo.Trimbox.left, pageInfo.Trimbox.bottom, pageInfo.Trimbox.width, pageInfo.Trimbox.height);
+                    p.stroke();
+
+                    double xOfs = pageInfo.Trimbox.left;
+                    double yOfs = pageInfo.Trimbox.bottom;
+
+                    decimal[] widths = _param.PartsWidth;
+
+                    if (_param.Mirrored && idx % 2 == 0)
                     {
-                        PdfPageInfo pageInfo = boxes[idx];
-
-                        p.begin_page_ext(pageInfo.Mediabox.width, pageInfo.Mediabox.height, "");
-
-                        int l_print = p.define_layer("print", "");
-                        int v_layer = p.define_layer("visual", "");
-
-                        if (fileAndSchema)
-                        {
-                            p.begin_layer(l_print);
-                            var page_handle = p.open_pdi_page(doc, idx + 1, "");
-                            p.fit_pdi_page(page_handle, 0, 0, "");
-                            p.close_pdi_page(page_handle);
-
-                            DrawFalcMarks(p, pageInfo, idx);
-                        }
-
-                        p.begin_layer(v_layer);
-                        int gstate = p.create_gstate("overprintmode=1 overprintfill=true overprintstroke=true");
-                        p.set_gstate(gstate);
-
-                        p.setcolor("fillstroke", "cmyk", markColor.C / 100, markColor.M / 100, markColor.Y / 100, markColor.K / 100);
-                        int spot = p.makespotcolor(markColor.Name);
-                        p.setcolor("stroke", "spot", spot, 1.0, 0.0, 0.0);
-                        p.setlinewidth(1.0);
-                        p.rect(pageInfo.Trimbox.left, pageInfo.Trimbox.bottom, pageInfo.Trimbox.width, pageInfo.Trimbox.height);
-                        p.stroke();
-
-                        double xOfs = pageInfo.Trimbox.left;
-                        double yOfs = pageInfo.Trimbox.bottom;
-
-                        decimal[] widths = _param.PartsWidth;
-
-                        if (_param.Mirrored && idx % 2 == 0)
-                        {
-                            widths = _param.PartsWidth.Reverse().ToArray();
-                        }
-                        DrawPage(p, pageInfo, xOfs, yOfs, widths);
-                        DrawDimension(p, spot, pageInfo, widths);
-
-                        // Тут має бути логіка створення схеми Falc
-                        // Використовуйте _param.Mirrored та _param.PartsWidth для налаштування схеми
-                        p.end_page_ext($"trimbox {{{pageInfo.Trimbox.left} {pageInfo.Trimbox.bottom} {pageInfo.Trimbox.width + pageInfo.Trimbox.left} {pageInfo.Trimbox.bottom + pageInfo.Trimbox.height}}}");
+                        widths = _param.PartsWidth.Reverse().ToArray();
                     }
+                    DrawPage(p, pageInfo, xOfs, yOfs, widths);
+                    DrawDimension(p, spot, pageInfo, widths);
 
-                    if (doc != -1)
-                    {
-                        p.close_pdi_document(doc);
-                    }
-
-                    p.end_document("");
+                    // Тут має бути логіка створення схеми Falc
+                    // Використовуйте _param.Mirrored та _param.PartsWidth для налаштування схеми
+                    p.end_page_ext($"trimbox {{{pageInfo.Trimbox.left} {pageInfo.Trimbox.bottom} {pageInfo.Trimbox.width + pageInfo.Trimbox.left} {pageInfo.Trimbox.bottom + pageInfo.Trimbox.height}}}");
                 }
-                catch (PDFlibException e)
+
+                if (doc != -1)
                 {
-                    PdfHelper.LogException(e, "FalcSchema");
+                    p.close_pdi_document(doc);
                 }
+
+                p.end_document("");
             }
+            catch (PDFlibException e)
+            {
+                PdfHelper.LogException(e, "FalcSchema");
+            }
+
         }
 
         private void DrawDimension(PDFlib p, int spot, PdfPageInfo pageInfo, decimal[] widths)
@@ -173,7 +176,7 @@ namespace JobSpace.Static.Pdf.Create.Falc
 
             var c = _param.Color;
 
-            p.setcolor("fillstroke", "cmyk", c.C/100, c.M/100, c.Y/100, c.K/100);
+            p.setcolor("fillstroke", "cmyk", c.C / 100, c.M / 100, c.Y / 100, c.K / 100);
             p.setlinewidth(1.5);
 
             double x = trimbox.left;
