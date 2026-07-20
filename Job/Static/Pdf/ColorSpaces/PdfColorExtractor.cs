804363fe993aa75c7d6f103a8d068906932dcf51
garymiller30 <garymiller30@gmail.com>
2026-07-20 10:11:40 +0300

    remove useless function

--- Job/Static/Pdf/ColorSpaces/PdfColorExtractor.cs
+++ Job/Static/Pdf/ColorSpaces/PdfColorExtractor.cs
@@ -34,41 +34,5 @@ namespace JobSpace.Static.Pdf.ColorSpaces
 
             return colors;
         }
-
-        public static List<string> ExtractColorsFromPage(string pdfPath, int pageNumber)
-        {
-            List<string> colors = new List<string>();
-            PdfDocument pdfDoc = null;
-
-            try
-            {
-                pdfDoc = new PdfDocument(new PdfReader(pdfPath));
-
-                if (pageNumber < 1 || pageNumber > pdfDoc.GetNumberOfPages())
-                {
-                    Console.WriteLine($"Помилка: Номер сторінки {pageNumber} недійсний для документа.");
-                    return colors; // Повернути порожній список
-                }
-
-                PdfPage page = pdfDoc.GetPage(pageNumber);
-                colors.AddRange(ExtractColorsFromPage(page));
-            }
-            catch (IOException ioEx)
-            {
-                Console.WriteLine($"Помилка читання файлу: {ioEx.Message}");
-                // Додаткова обробка помилки, якщо потрібно
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Сталася помилка під час обробки PDF: {ex.Message}");
-                // Додаткова обробка помилки
-            }
-            finally
-            {
-                pdfDoc?.Close(); // Завжди закриваємо документ
-            }
-
-            return colors;
-        }
     }
 }
